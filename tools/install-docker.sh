395bc670b5df44a1aef35df7b58308a4af197f9b
Wu Zhangjin <wuzhangjin@gmail.com>
2016-10-12 03:54:41 +0800

    tools: Fix up tools/install-docker.sh
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- tools/install-docker.sh
+++ tools/install-docker.sh
@@ -10,6 +10,7 @@ version=`sed -n -e "/ main/p" /etc/apt/sources.list | grep -v ^# | head -1 | cut
 sudo bash -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-'${version}' main" > /etc/apt/sources.list.d/docker.list'
 sudo apt-get -y update
 sudo apt-get -y install apt-transport-https ca-certificates
+sudo apt-get -y install bridge-utils
 sudo apt-get -y --force-yes install docker-engine
 sudo usermod -aG docker $USER
 

64810b42fa3e35245cd8ee1f976dda9f38ee060a
Wu Zhangjin <wuzhangjin@gmail.com>
2017-08-12 22:16:48 +0800

    tools: macaddr: generate valid mac addr
    The 2nd number must be even number.
    
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- tools/random/macaddr.sh
+++ tools/random/macaddr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-dd bs=1 count=6 if=/dev/random 2>/dev/null | od -h | head -1 | cut -d' ' -f2,3,4 | sed -e "s/\(..\)\(..\) \(..\)\(..\) \(..\)\(..\)/\1:\2:\3:\4:\5:\6/g" | tr -d '\n'
+dd bs=1 count=6 if=/dev/random 2>/dev/null | od -h | head -1 | cut -d' ' -f2,3,4 | sed -e "s/\(.\)[0-9a-f]\(..\) \(..\)\(..\) \(..\)\(..\)/\10:\2:\3:\4:\5:\6/g" | tr -d '\n'

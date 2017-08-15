467134ce965f0b15aae70c35d2fff4d94a6b80df
Wu Zhangjin <wuzhangjin@gmail.com>
2017-08-15 22:16:48 +0800

    sharing: only enable sharing while SHARE=1 pass
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- system/tools/sharing/start.sh
+++ system/tools/sharing/start.sh
@@ -6,8 +6,11 @@
 SHARE_TAG="$(cat /proc/cmdline | tr ' ' '\n' | grep ^sharetag= | cut -d'=' -f2 | tr ',' ' ')"
 SHARE_DIR="$(cat /proc/cmdline | tr ' ' '\n' | grep ^sharedir= | cut -d'=' -f2 | tr ',' ' ')"
 
+# Must pass sharetag via command line?
+[ -z "$SHARE_TAG" ] && exit 0
+
+#[ -z "$SHARE_TAG" ] && SHARE_TAG=hostshare
 [ -z "$SHARE_DIR" ] && SHARE_DIR=/hostshare/
-[ -z "$SHARE_TAG" ] && SHARE_TAG=hostshare
 
 [ ! -d $SHARE_DIR ] && mkdir -p $SHARE_DIR
 mount -t 9p -o trans=virtio $SHARE_TAG $SHARE_DIR
--- system/tools/sharing/stop.sh
+++ system/tools/sharing/stop.sh
@@ -4,6 +4,10 @@
 #
 
 SHARE_DIR="$(cat /proc/cmdline | tr ' ' '\n' | grep ^sharedir= | cut -d'=' -f2 | tr ',' ' ')"
+SHARE_TAG="$(cat /proc/cmdline | tr ' ' '\n' | grep ^sharetag= | cut -d'=' -f2 | tr ',' ' ')"
+
+# Must pass sharetag via command line?
+[ -z "$SHARE_TAG" ] && exit 0
 
 [ -z "$SHARE_DIR" ] && SHARE_DIR=/hostshare/
 

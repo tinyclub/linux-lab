ce8005f87b64abb8cb170d6515798763de436353
falcon <wuzhangjin@gmail.com>
2017-09-05 22:16:48 +0800

    uboot: only hack ip when docker0 net is 10.66.33.n
    /etc/default/docker:
    
    	DOCKER_OPTS="$DOCKER_OPTS --bip=10.66.33.10/24"
    
    Signed-off-by: falcon <wuzhangjin@gmail.com>

--- tools/uboot/env.sh
+++ tools/uboot/env.sh
@@ -10,14 +10,14 @@ BOOT_CMD=${U_BOOT_CMD}
 IMAGES_DIR=${TFTPBOOT}
 UBOOT_IMAGE=${BIMAGE}
 
-# Fixme: this not work on some platforms, just skip this script currently.
-# Todo: load env vars from a storage instead of compile it with uboot image
-exit 0
-
 if [ -f "$UBOOT_IMAGE" ]; then
-  ## Fix up tftp server ip
-  sed -i -e "s/route=[0-9.]* /route=$ROUTE /g" -ur $UBOOT_IMAGE
-  sed -i -e "s/serverip [0-9.]*;/serverip $ROUTE;/g" -ur $UBOOT_IMAGE
+  ## Fix up tftp server ip, only when the docker net is 10.66.33.n
+  ## See /etc/default/docker: DOCKER_OPTS="$DOCKER_OPTS --bip=10.66.33.10/24"
+  echo $ROUTE | grep -q 10.66.33
+  if [ $? -eq 0 ]; then
+    sed -i -e "s/route=[0-9.]* /route=$ROUTE /g" -ur $UBOOT_IMAGE
+    sed -i -e "s/serverip [0-9.]*;/serverip $ROUTE;/g" -ur $UBOOT_IMAGE
+  fi
   ## Fix up boot command
   sed -i -e "s/run bootcmd[0-9]*;/run $BOOT_CMD;/g" -ur $UBOOT_IMAGE
 fi

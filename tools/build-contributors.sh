d4686a6255d6445a9f3e281a87b0765c15a3b367
Wu Zhangjin <wuzhangjin@gmail.com>
2017-12-29 22:16:48 +0800

    tools: contributors.sh: add filter argument
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- tools/build-contributors.sh
+++ tools/build-contributors.sh
@@ -6,4 +6,6 @@
 
 TOP_DIR=$(cd $(dirname $0) && pwd)
 
-grep "^author*:" -ur $TOP_DIR/../_posts/ | cut -d':' -f3 | sed -e "s/^ '*//g" | sed -e "s/'$//g" | sort | uniq -c | sort -k1 -g -r
+filter="*$1*"
+
+grep "^author*:" -ur $TOP_DIR/../_posts/$filter | cut -d':' -f3 | sed -e "s/^ '*//g" | sed -e "s/'$//g" | sort | uniq -c | sort -k1 -g -r

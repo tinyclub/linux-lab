4199b21a4f6a2865319ba98b6d8c28975b1ce55c
Wu Zhangjin <wuzhangjin@gmail.com>
2019-05-12 23:31:13 -0700

    tools: rootfs: docker: run.sh: update
    1. not pull by default
    2. map local linux-lab to /linux-lab, to share files easily
    
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- tools/rootfs/docker/run.sh
+++ tools/rootfs/docker/run.sh
@@ -16,15 +16,24 @@ arch=$2
 tmpdir=$(echo $image | tr '/' '-' | tr ':' '-')
 rootdir=$PREBUILT_FULLROOT/tmp/$tmpdir
 qemu_user_static=/usr/bin/qemu-$arch-static
-entry=/bin/bash
+qemumap="-v $qemu_user_static:$qemu_user_static"
+
+# Enable sharing /linux-lab by default, to extrat container, please disable with SHARE=0
+[ -z "$SHARE" ] && SHARE=1
+[ $SHARE -eq 1 ] && sharemap="-v $TOP_DIR:/linux-lab"
+
+mapping=" $qemumap $sharemap "
 
 [ -z "$image" -o -z "$arch" ] && echo "Usage: $0 image arch" && exit 1
 
 which $qemu_user_static 2>&1 > /dev/null
 [ $? -ne 0 ] && echo "LOG: Install qemu-user-static at first" && exit 1
 
-echo "LOG: Pulling $image"
-docker pull $image
+[ -z "$PULL" ] && PULL=0
+if [ $PULL -eq 1 ]; then
+    echo "LOG: Pulling $image"
+    docker pull $image
+fi
 
 echo "LOG: Running $image"
-docker run -it -v $qemu_user_static:$qemu_user_static $image $entry
+docker run -it $mapping $image

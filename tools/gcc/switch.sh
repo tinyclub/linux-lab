57415d6369f00c859387d3df94e579d44b6ba10d
Wu Zhangjin <wuzhangjin@gmail.com>
2019-05-28 19:12:48 -0700

    switch: update for user experience
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- tools/gcc/switch.sh
+++ tools/gcc/switch.sh
@@ -10,18 +10,27 @@
 arch=$1
 version=$2
 
-[ -z "$arch" ] && "Usage: $0 ARCH VERSION" && exit 1
-[ -z "$version" ] && "Usage: $0 ARCH VERSION" && exit 1
+[ -z "$arch" ] && echo "Usage: $0 ARCH GCC_VERSION" && exit 1
 
 if [ ${arch} != "x86" ]; then
     name=${arch}-linux-gnu-gcc
     [ "$arch" == "arm" ] && name=${arch}-linux-gnueabi-gcc
+    [ "$arch" == "aarch64" ] && name=${arch}-linux-gnu-gcc
     [ "$arch" == "mips" ] && name=mipsel-linux-gnu-gcc
 else
     name=gcc
 fi
+
+if [ -z "$version" ]; then
+    echo -e "\nLOG: Available gcc versions:\n"
+    update-alternatives --list $name
+
+    echo -e "\nUsage: $0 ARCH GCC_VERSION"
+    exit 1
+fi
+
 path=/usr/bin/${name}-${version}
 
 [ ! -f $path ] && echo "ERROR: No such file: $path" && exit 1
 
-update-alternatives --set ${name} ${path}
+update-alternatives --verbose --set ${name} ${path}

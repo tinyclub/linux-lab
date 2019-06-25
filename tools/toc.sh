1832faed0946b6a0fdad7f8b62850d287d240ccd
Wu Zhangjin <wuzhangjin@gmail.com>
2019-06-24 17:11:29 -0700

    tools: toc.sh: accept argument
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- tools/toc.sh
+++ tools/toc.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-cat README.md | grep ^# | sed "s%^#####%        -%g" | sed "s%^####%      -%g" | sed -e "s%^###%   -%g" | sed -e "s%^## %- %g" | grep -v ^# \
+
+TOP_DIR=$(cd $(dirname $0)/../ && pwd)
+
+[ -n "$1" ] && README=$1
+[ -z "$README" ] && README=$TOP_DIR/README.md
+
+cat $README | grep -v "^## Contents" | grep ^# | sed "s%^#####%        -%g" | sed "s%^####%      -%g" | sed -e "s%^###%   -%g" | sed -e "s%^## %- %g" | grep -v ^# \
 	| sed -e "s%\(.*\)- \(.*\)%echo -n \"\1- [\2]\";echo \"(#\L\2)\" | tr ' ' '-' | tr -d '/' | tr -d ':'%g" | bash

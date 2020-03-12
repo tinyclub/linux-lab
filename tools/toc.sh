ddace0aa1ccb64bae22c8ee29b93d9ecb29db09e
Wu Zhangjin <wuzhangjin@gmail.com>
2020-03-12 17:33:09 +0800

    update tools/toc.sh
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- tools/toc.sh
+++ tools/toc.sh
@@ -6,7 +6,22 @@ TOP_DIR=$(cd $(dirname $0)/../ && pwd)
 [ -n "$1" ] && README=$1
 [ -z "$README" ] && README=$TOP_DIR/README.md
 
+echo -e "<!-- toc start -->\n" > ${README}.toc
+
+echo $README | grep -q _zh
+if [ $? -eq 0 ]; then
+  echo -e "\n# 目录\n" >> ${README}.toc
+else
+  echo -e "\n# Table of Content\n" >> ${README}.toc
+fi
+
 cat $README | grep -v "^## Contents" | grep ^# | sed "s%^#####%        -%g" | sed "s%^####%      -%g" | sed -e "s%^###%   -%g" | sed -e "s%^## %- %g" | grep -v ^# \
-	| sed -e "s%\(.*\)- \(.*\)%echo -n \"\1- [\2]\";echo \"(#\L\2)\" | tr ' ' '-' | tr -d '/' | tr -d '.' | tr -d ':'%g" | bash -v >${README}.toc 2>/dev/null
+	| sed -e "s%\(.*\)- \(.*\)%echo -n \"\1- [\2]\";echo \"(#\L\2)\" | tr ' ' '-' | tr -d '/' | tr -d '.' | tr -d ':'%g" | bash -v >>${README}.toc 2>/dev/null
+
+echo -e "\n<!-- toc end -->" >> ${README}.toc
 
 echo "${README}.toc generated."
+
+sed -i -e '/<!-- toc start -->/{:a; N; /\n<!-- toc end -->$/!ba; r '${README}'.toc' -e 'd;}' ${README}
+
+echo "${README} updated with new toc."

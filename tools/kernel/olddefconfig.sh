73183226b57821df27da7beef92d439177aed869
Wu Zhangjin <wuzhangjin@gmail.com>
2020-03-14 03:54:55 +0800

    tools: add missing kernel/olddefconfig.sh
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

---
+++ tools/kernel/olddefconfig.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# olddefconfig.sh -- get available olddefconfig target
+#
+
+makefile=$1
+
+oldconfig=olddefconfig
+
+if [ -n "$makefile" -a -f "$makefile" ]; then
+  grep olddefconfig -q $makefile
+  if [ $? -ne 0 ]; then
+    grep oldnoconfig -q $makefile
+    if [ $? -ne 0 ]; then
+      oldconfig=oldconfig
+    else
+      oldconfig=oldnoconfig
+    fi
+  fi
+fi
+
+echo $oldconfig

c3de1cfae8cec0e1bf791bb2a4b40c7d9d93367f
Wu Zhangjin <wuzhangjin@gmail.com>
2020-03-28 13:56:44 +0800

    tools/toc.sh: add missing first level toc
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- tools/toc.sh
+++ tools/toc.sh
@@ -15,7 +15,7 @@ else
   echo -e "\n# Table of Content\n" >> ${README}.toc
 fi
 
-cat $README | grep -v "^## Contents" | grep ^# | sed "s%^#####%        -%g" | sed "s%^####%      -%g" | sed -e "s%^###%   -%g" | sed -e "s%^## %- %g" | grep -v ^# \
+cat $README | grep -v "^# Table of Content" | grep -v "^# 目录" | grep ^# | sed "s%^#####%            - %g" | sed "s%^####%          - %g" | sed -e "s%^###%       - %g" | sed -e "s%^## %    - %g" | sed -e "s%^# %- %g"\
 	| sed -e "s%\(.*\)- \(.*\)%echo -n \"\1- [\2]\";echo \"(#\L\2)\" | tr ' ' '-' | tr -d '/' | tr -d '.' | tr -d ':'%g" | bash -v >>${README}.toc 2>/dev/null
 
 echo -e "\n<!-- toc end -->" >> ${README}.toc

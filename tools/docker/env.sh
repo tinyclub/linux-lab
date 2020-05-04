8ecce7941afd9a8aad46f8516879c1c12f6717e5
Wu Zhangjin <wuzhangjin@gmail.com>
2020-05-04 20:46:32 +0800

    tools: docker/env.sh: add windows support
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- tools/docker/env.sh
+++ tools/docker/env.sh
@@ -13,10 +13,21 @@ fi
 docker --version 2>&1 >/dev/null
 [ $? -ne 0 ] && echo "ERR: No docker installed?" && exit 2
 
-# Hardware
-echo "Product: `cat /sys/class/dmi/id/product_name`"
+# Get host os system
+HOST_OS=$(uname)
 
-echo "Board: `cat /sys/class/dmi/id/board_name`"
+# Hardware
+if [ "x$HOST_OS" = "xDarwin" ]; then
+  echo "TBD";
+elif [ "x$HOST_OS" = "xLinux" ]; then
+  vendor="`cat /sys/class/dmi/id/board_vendor`"
+  board="`cat /sys/class/dmi/id/board_name`"
+  product="`cat /sys/class/dmi/id/product_name`"
+  echo "Product: $vendor, $board, $product"
+else # Windows
+  product=`systeminfo.exe | head -15 | tail -3 | cut -d ':' -f2 | tr -s ' ' | tr '\n' ',' | tr -d -c "[:print:]" | sed -e 's/ ,/,/g;s/,$//g'`
+  echo "Product:$product"
+fi
 
 echo "ARCH: `uname -m`"
 
@@ -24,13 +35,19 @@ cpuinfo=`cat /proc/cpuinfo | grep 'model name' | head -1 | cut -d':' -f2 | tr -s
 cpunum=`cat /proc/cpuinfo | grep 'model name' | wc -l`
 echo "CPU: $cpunum x$cpuinfo"
 
-mem_size=`cat /proc/meminfo | egrep 'MemTotal' | tr -s ' ' | cut -d ' ' -f2 | xargs -i echo "scale=0;{}/1024" | bc -l`
+mem_size=`cat /proc/meminfo | egrep 'MemTotal' | tr -s ' ' | cut -d ' ' -f2 | xargs -i sh -c 'echo $(({}/1024))'`
 echo "RAM: $mem_size MiB"
 
 # System
-system_desc="`lsb_release -d | cut -d':' -f2 | tr -d '\t'`"
-system_code="`lsb_release -c | cut -d':' -f2 | tr -d '\t'`"
-echo "System: $system_desc, $system_code"
+if [ "x$HOST_OS" = "xDarwin" ]; then
+  echo "TBD";
+elif [ "x$HOST_OS" = "xLinux" ]; then
+  system_desc="`lsb_release -d | cut -d':' -f2 | tr -d '\t'`"
+  system_code="`lsb_release -c | cut -d':' -f2 | tr -d '\t'`"
+  echo "System: $system_desc, $system_code"
+else # Windows
+  echo "System: $HOST_OS, `uname -n`"
+fi
 
 # Kernel
 echo "Linux: `uname -r`"

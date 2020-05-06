0eeb0ccc4b54643fcb140fc61e16890c3a1f680c
Wu Zhangjin <wuzhangjin@gmail.com>
2020-05-06 21:44:13 +0800

    tools: docker/env.sh: share it with the other labs
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- tools/docker/env.sh
+++ tools/docker/env.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 #
-# env.sh -- list current system information of running docker
+# env -- list current system information of running docker
 #
+# Copyright (C) 2016-2020 Wu Zhangjin <lzufalcon@163.com>
+#
+
+uname | grep -q MINGW && PWD_OPT="-W"
+CLOUD_LAB_DIR="$(cd "$(dirname "$0")"/../../../../ && pwd $PWD_OPT)"
 
 # dump system information of lab is not meaningful
 if [ -d '/configs' ]; then
@@ -9,80 +14,15 @@ if [ -d '/configs' ]; then
     exit 1
 fi
 
-# must already install docker
-docker --version 2>&1 >/dev/null
-[ $? -ne 0 ] && echo "ERR: No docker installed?" && exit 2
+ENV_TOOL=$CLOUD_LAB_DIR/tools/docker/env
 
-# Get host os system
-HOST_OS=$(uname)
-
-# Hardware
-if [ "x$HOST_OS" = "xDarwin" ]; then
-  # ref: http://teczd.com/2015/09/23/osx-get-system-info-from-command-line/
-  product=`system_profiler SPHardwareDataType | grep -i "Model" | cut -d ':' -f2 | tr '\n' ',' | sed -e 's/ ,    /,/g;s/,$//g'`
-  echo "Product: Apple,$product";
-elif [ "x$HOST_OS" = "xLinux" ]; then
-  [ -f /sys/class/dmi/id/board_vendor ] && vendor="`cat /sys/class/dmi/id/board_vendor`"
-  [ -f /sys/class/dmi/id/board_name ] && board="`cat /sys/class/dmi/id/board_name`"
-  product="`cat /sys/class/dmi/id/product_name`"
-  echo "Product: $vendor, $board, $product"
-else # Windows
-  product=`systeminfo.exe | head -15 | tail -3 | cut -d ':' -f2 | tr -s ' ' | tr '\n' ',' | tr -d -c "[:print:]" | sed -e 's/ ,/,/g;s/,$//g'`
-  echo "Product:$product"
-fi
-
-echo "ARCH: `uname -m`"
-
-if [ "x$HOST_OS" = "xDarwin" ]; then
-  # sysctl -a | grep machdep
-  cpuinfo=" `sysctl -n machdep.cpu.brand_string`"
-  cpunum=`sysctl -n hw.physicalcpu`
+if [ -f $ENV_TOOL ]; then
+  $ENV_TOOL
 else
-  cpuinfo=`cat /proc/cpuinfo | grep 'model name' | head -1 | cut -d':' -f2 | tr -s ' '`
-  cpunum=`cat /proc/cpuinfo | grep 'model name' | wc -l`
+  echo "LOG: $ENV_TOOL not exist, please update your cloud lab:"
+  echo
+  echo "    $ cd $CLOUD_LAB_DIR"
+  echo "    $ git pull"
+  echo "    $ ls tools/docker/env.sh"
+  echo
 fi
-
-echo "CPU: $cpunum x$cpuinfo"
-
-if [ "x$HOST_OS" = "xDarwin" ]; then
-  # sysctl -n hw.memsize
-  mem_size="`system_profiler SPHardwareDataType | grep Memory | cut -d':' -f2 | tr -s ' '`"
-  echo "RAM:$mem_size"
-else
-  mem_size=`cat /proc/meminfo | egrep 'MemTotal' | tr -s ' ' | cut -d ' ' -f2 | xargs -I{} sh -c 'echo $(({}/1024))'`
-  echo "RAM: $mem_size MiB"
-fi
-
-# System
-if [ "x$HOST_OS" = "xDarwin" ]; then
-  echo "System:`sw_vers | cut -d ':' -f2 | tr '\t' ' ' | tr '\n' ',' | sed -e 's/,$//g'`"
-elif [ "x$HOST_OS" = "xLinux" ]; then
-  system_desc="`lsb_release -d | cut -d':' -f2 | tr -d '\t'`"
-  system_code="`lsb_release -c | cut -d':' -f2 | tr -d '\t'`"
-  echo "System: $system_desc, $system_code"
-else # Windows
-  system=`systeminfo.exe | head -4 | tail -3 | cut -d ':' -f2 | tr -s ' ' | tr '\n' ',' | tr -d -c "[:print:]" | sed -e 's/ ,/,/g;s/,$//g'`
-  echo "System:$system"
-fi
-
-# Kernel
-echo "Kernel: `uname -sr`"
-
-# Docker
-echo "Docker: `docker --version`"
-
-# Shell
-shell=`basename $SHELL`
-case $shell in
-  bash)
-       shell_version=$BASH_VERSION
-       ;;
-   zsh)
-       shell_version=$ZSH_VERSION
-       ;;
-   *)
-       shell_version=
-       ;;
-esac
-
-echo "Shell: $SHELL $shell_version"

c5908644e3459dc9c9b5855dd8078cea0e8a50e0
Wu Zhangjin <falcon@tinylab.org>
2020-12-28 04:20:36 +0800

    tools: getip.py: make it more robust
    Signed-off-by: Wu Zhangjin <falcon@tinylab.org>

--- tools/helper/getip.py
+++ tools/helper/getip.py
@@ -15,26 +15,63 @@ import sys
 
 serial_port = sys.argv[1] if len(sys.argv) > 1 else '/dev/ttyUSB0'
 serial_baudrate = sys.argv[2] if len(sys.argv) > 2 else '115200'
+login_user = sys.argv[3] if len(sys.argv) > 3 else 'debian'
+login_pass = sys.argv[4] if len(sys.argv) > 4 else 'linux-lab'
+run_cmd = sys.argv[5] if len(sys.argv) > 5 else "ifconfig | grep 'inet ' | tr -d -c '^[0-9. ]' | awk '{print $1}'"
 
 # configure the serial connections (the parameters differs on the device you are connecting to)
 ser = serial.Serial(
     port = serial_port,
     baudrate = serial_baudrate,
-    rtscts=True,
-    dsrdtr=True,
+    rtscts = True,
+    dsrdtr = True,
     timeout = 1
 )
 
 ser.isOpen()
 
 # send the character to the device
-# (note that I happend a \r\n carriage return and line feed to the characters - this is requested by my device)
-ser.write(b"ifconfig | grep 'inet ' | tr -d -c '^[0-9. ]' | awk '{print $1}'\r\n")
+ser.write(b"\n")
 out = b''
 # let's wait one second before reading output (let's give device time to answer)
-time.sleep(1)
+time.sleep(0.1)
 while ser.inWaiting() > 0:
     out += ser.read(1)
 
-if out != '':
-    print (out.decode("utf-8").split("\r\n")[1])
+if (out.decode("utf-8").find("$ ")) == -1:
+    while out.decode("utf-8").find("login:") == -1:
+        out = b''
+        time.sleep(0.2)
+        while ser.inWaiting() > 0:
+            out += ser.read(1)
+        #if out != '':
+        #    print (out.decode("utf-8"))
+
+    ser.write(login_user.encode() + b"\r\n")
+    while out.decode("utf-8").find("Password:") == -1:
+        out = b''
+        time.sleep(0.2)
+        while ser.inWaiting() > 0:
+            out += ser.read(1)
+        #if out != '':
+        #    print (out.decode("utf-8"))
+
+    ser.write(login_pass.encode() + b"\r\n")
+    while out.decode("utf-8").find("$ ") == -1:
+        out = b''
+        time.sleep(0.5)
+        while ser.inWaiting() > 0:
+            out += ser.read(1)
+        #if out != '':
+        #    print (out.decode("utf-8"))
+
+ser.write(run_cmd.encode("utf-8") + b"\r\n")
+out = b''
+while out.decode("utf-8").find("$ ") == -1:
+    out = b''
+    time.sleep(0.2)
+    while ser.inWaiting() > 0:
+        out += ser.read(1)
+
+    if out != '':
+        print (out.decode("utf-8").split("\r\n")[1])

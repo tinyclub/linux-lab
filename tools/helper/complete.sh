9849e691c1f5e3bc49b222e19cc79016046c58ce
Wu Zhangjin <falcon@tinylab.org>
2020-12-30 11:33:51 +0800

    tools: complete.sh: add board/BOARD completion
    Signed-off-by: Wu Zhangjin <falcon@tinylab.org>

--- tools/helper/complete.sh
+++ tools/helper/complete.sh
@@ -4,6 +4,10 @@
 #
 # Copyright (C) 2020 Wu Zhangjin <lzufalcon@163.com>
 #
+
+# Allow commands with '='
+COMP_WORDBREAKS="$(echo $COMP_WORDBREAKS | tr -d '=') "
+
 function _makefile_targets {
     local curr_arg;
     local targets;
@@ -13,14 +17,16 @@ function _makefile_targets {
     case $COMP_CWORD in
          2)
             if [[ -e "$(pwd)/Makefile" ]]; then
+                last_arg=${COMP_WORDS[COMPCWORD-1]}
                 common_targets="$(grep "^APP_TARGETS :=" $(pwd)/Makefile | cut -d '=' -f2)"
-                echo $common_targets | tr ' ' '\n' | grep -q "^${COMP_WORDS[COMP_CWORD-1]}$"
+                echo $common_targets | tr ' ' '\n' | grep -q "^$last_arg$"
+
                 # FXIME: Not all of the packages support 'all'
                 if [ $? -eq 0 ]; then
                   targets="all $(grep "^APPS :=" $(pwd)/Makefile | cut -d '=' -f2)"
                 else
                   echo
-                  echo "LOG: No such command matched: ${COMP_WORDS[COMP_CWORD-1]}"
+                  echo "LOG: No such command matched: "$last_arg""
                   return 0
                 fi
             fi
@@ -29,6 +35,8 @@ function _makefile_targets {
             # Find makefile targets available in the current directory
             ignores="^_|-km|features|kernel-modules|module-|module$|FORCE|rootdir|default-"
             if [[ -e "$(pwd)/Makefile" ]]; then
+                BOARDS="$(find $(pwd)/boards/ -maxdepth 3 -name "Makefile" -exec egrep -H "^_BASE|^_PLUGIN" {} \; | tr -s '/' | egrep ".*" | sort -t':' -k2 | cut -d':' -f1 | egrep -v "/module" | sed -e "s%$(pwd)/boards/\(.*\)/Makefile%BOARD=\1%g")"
+                boards="$(find $(pwd)/boards/ -maxdepth 3 -name "Makefile" -exec egrep -H "^_BASE|^_PLUGIN" {} \; | tr -s '/' | egrep ".*" | sort -t':' -k2 | cut -d':' -f1 | egrep -v "/module" | sed -e "s%$(pwd)/boards/\(.*\)/Makefile%board=\1%g")"
                 common_targets="$(grep "^APP_TARGETS :=" $(pwd)/Makefile | cut -d '=' -f2)"
                 all_apps="$(grep "^APPS :=" $(pwd)/Makefile | cut -d '=' -f2)"
                 app_targets=''
@@ -40,7 +48,7 @@ function _makefile_targets {
                     done
                 done
                 default_targets="$(egrep -e '^[a-zA-Z0-9_-]+:[^=]*' $(pwd)/Makefile | grep -v ":=" | cut -d ':' -f1 | egrep -v "$ignores" | tr '\n' ' ')"
-                targets="$common_targets $all_apps $default_targets"
+                targets="${boards} ${BOARDS} $common_targets $all_apps $default_targets"
             fi
             ;;
     esac

b7f5002b17d617b5f09b1f1af3e1b1e8f1ba7b18
Wu Zhangjin <falcon@tinylab.org>
2021-02-01 06:15:10 +0800

    tools: showterm is installed in latest docker image
    Signed-off-by: Wu Zhangjin <falcon@tinylab.org>

--- tools/helper/showterm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# showterm.sh -- install showterm for command line recording
-#
-
-sudo apt-get -y update
-sudo apt-get install -y curl
-sudo sh -c 'curl showterm.io/showterm > /usr/bin/showterm'
-sudo chmod a+x /usr/bin/showterm

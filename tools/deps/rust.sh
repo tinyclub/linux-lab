e1384e194e0c9e664ac9f20f1d7d1a28a29a1f55
Wu Zhangjin <falcon@tinylab.org>
2021-09-25 00:32:21 +0800

    feature: rust: clean up mirror site url
    Signed-off-by: Wu Zhangjin <falcon@tinylab.org>

--- tools/deps/rust.sh
+++ tools/deps/rust.sh
@@ -42,7 +42,7 @@ EOF
 #       RUSTUP_DIST_SERVER=https://mirrors.sjtug.sjtu.edu.cn/rust-static/
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | \
   RUSTUP_UPDATE_ROOT=https://mirrors.sjtug.sjtu.edu.cn/rust-static/rustup \
-  RUSTUP_DIST_SERVER=https://mirrors.sjtug.sjtu.edu.cn/rust-static/ \
+  RUSTUP_DIST_SERVER=https://mirrors.sjtug.sjtu.edu.cn/rust-static \
   sh -s -- -y -v --default-toolchain 1.54-x86_64-unknown-linux-gnu --profile minimal -c rust-src,rustfmt && \
-  echo "RUSTUP_DIST_SERVER=https://mirrors.sjtug.sjtu.edu.cn/rust-static/" >> $HOME/.cargo/env && \
+  echo "RUSTUP_DIST_SERVER=https://mirrors.sjtug.sjtu.edu.cn/rust-static" >> $HOME/.cargo/env && \
   bash -c "source $HOME/.cargo/env && cargo install --locked --version 0.56.0 bindgen"

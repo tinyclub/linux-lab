2931eb09d09e41a33db21eba33ce0432361307d5
Wu Zhangjin <falcon@tinylab.org>
2022-01-05 22:12:37 +0800

    testing: fix up variables parse
    read out the string patterns like this:
    
        a=b
        a='do something ...'
    
    ignore the single arguments like: rw, ro, quiet, splash.
    
    the script firstly get patterns like a='do something', second, get
    patterns like a=b.
    
    Signed-off-by: Wu Zhangjin <falcon@tinylab.org>

--- src/system/tools/testing/start.sh
+++ src/system/tools/testing/start.sh
@@ -15,7 +15,7 @@
 # Keep: test_case=ls\ /root    test_case="ls /root,echo hello world"
 #
 set -a
-eval "$(cat /proc/cmdline | tr ' ' '\n' | egrep -v '\..*=|^$' | tr '\n' ' ' | tr '=' '\n' | sed -e '/["\\]/!s/\(.*\) [a-zA-Z_]\{1,\} \(.*\)/\1 \2/g' | tr '\n' '=' | tr ' ' '\n' | egrep -v '\..*=|^$' | tr '\n' ' ' | tr '=' '\n' | sed -e '/["\\]/!s/\(.*\) [a-zA-Z_]\{1,\} \(.*\)/\1 \2/g' | tr '\n' '=' | tr ' ' '\n' | egrep -v '\..*=|^$' | tr '\n' ' ' | tr '=' '\n' | sed -e '/["\\]/!s/\(.*\) [a-zA-Z_]\{1,\} \(.*\)/\1 \2/g' | tr '\n' '=')"
+eval "$(cat /proc/cmdline | sed -e "s/^/ /g;s/$/ /g;s/\([^=' ]\{1,\}='[^']\{1,\}'\) /\n\1\n/g;s/ /  /g;s/ \([^=' ]\{1,\}=[^' ]\{1,\}\) /\n\1\n/g;s/  / /g" | grep '=')"
 set +a
 
 FEATURE="$feature"

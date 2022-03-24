10694eef70095396cd604ab80af4ef8562a1dd77
Wu Zhangjin <falcon@tinylab.org>
2022-03-24 20:19:46 +0800

    examples: add python math example with sympy
    Signed-off-by: Wu Zhangjin <falcon@tinylab.org>

---
+++ src/examples/python/README.md
@@ -0,0 +1,21 @@
+
+# Hello Python
+
+## Math
+
+### Sympy
+
+Solve the equations with the sympy module.
+
+    $ cd math/sympy
+    $ make
+    $ ./equ-solve.py
+    result:  [{x: 0, y: 0}, {x: 5, y: 5}]
+
+    $ ls equ-solve.jpg
+    -rw-r--r-- 1 kali kali 55769 Mar 24 20:16 equ-solve.jpg
+
+## References
+
+* [Sympy Solvers](https://docs.sympy.org/latest/tutorial/solvers.html)
+* [Sympy Plotting](https://docs.sympy.org/latest/modules/plotting.html)
---
+++ src/examples/python/math/sympy/equ-solve.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+#
+# equ-solve.py -- solve math equations
+#
+# 1. x^2 + (y-5)^2 = 5^2
+# 2. y = x
+#
+
+from sympy.plotting import *
+from sympy.abc import *
+from sympy import *
+
+# solve it directly
+result = solve([Eq(x**2 + (y-5)**2, 5**2), Eq(y, x)])
+print("result: ", result)
+
+# solve it with ploting
+p1 = plot_implicit(Eq(x**2 + (y-5)**2, 5**2), (x, -15, 15), (y, -15, 15), line_color='red', depth = 1, show = False, margin = 10)
+p2 = plot_implicit(Eq(y, x), (x, -10, 10), (y, -10, 10), depth = 1, line_color='blue', show = False)
+p1.extend(p2)
+
+p1.title = "Equations: 1. x^2 + (y-5)^2 = 5^2; 2. y = x\n\n"
+p1.size = (12, 12)
+p1.show()
+p1.save("equ-solve.jpg")

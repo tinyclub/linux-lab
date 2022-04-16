e3033590e6ce8998ec41905b38180a3abc26ee54
Wu Zhangjin <falcon@tinylab.org>
2022-04-17 03:17:34 +0800

    assembly: arm: call thumb code directly
    Signed-off-by: Wu Zhangjin <falcon@tinylab.org>

--- src/examples/assembly/arm/arm-hello.s
+++ src/examples/assembly/arm/arm-hello.s
@@ -1,4 +1,6 @@
+# thumb code are added in arm assembly as demonstration
 
+    /* .arm @ .code 32 */
     .text
     .globl _start
 
@@ -11,10 +13,20 @@ _start:
     mov     %r7, $4     /* write is syscall #4 */
     swi     $0          /* invoke syscall */
 
+    blx     _thumb      /* call thumb from arm, just for demo, not really required for pure arm mode */
+
                         /* syscall exit(int status) */
     mov     %r0, $0     /* status -> 0 */
     mov     %r7, $1     /* exit is syscall #1 */
     swi     $0          /* invoke syscall */
+
+    /* thumb code begin, just for demo */
+    .thumb @ .code 16
+    .thumb_func         /* must tell gcc the func is thumb func */
+_thumb:
+    mov    %r0, $0      /* do whatevery you want here */
+    bx     %lr          /* back from thumb to arm, we can simply use `blx _label` too */
+    /* thumb code end */
 
     .section .rodata    /* rodata section*/
 msg:

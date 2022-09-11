fc1c0a8cfc2fbf5e71eb75b2f366d55d039f63f8
Wu Zhangjin <falcon@tinylab.org>
2022-09-12 02:59:40 +0800

    tools: fix up toc indent
    Signed-off-by: Wu Zhangjin <falcon@tinylab.org>

--- tools/toc.sh
+++ tools/toc.sh
@@ -14,12 +14,12 @@ echo -e "<!-- toc start -->\n" > ${README}.toc
 
 echo $README | grep -q _zh
 if [ $? -eq 0 ]; then
-  echo -e "\n# 目录\n" >> ${README}.toc
+  echo -e "# 目录\n" >> ${README}.toc
 else
-  echo -e "\n# Table of Content\n" >> ${README}.toc
+  echo -e "# Table of Content\n" >> ${README}.toc
 fi
 
-cat $README | grep -v "^# Table of Content" | grep -v "^# 目录" | grep ^# | sed "s%^##### %          - %g" | sed "s%^#### %        - %g" | sed -e "s%^### %    - %g" | sed -e "s%^## %  - %g" | sed -e "s%^# %- %g"\
+cat $README | grep -v "^# Table of Content" | grep -v "^# 目录" | grep ^# | sed "s%^##### %        - %g" | sed "s%^#### %      - %g" | sed -e "s%^### %    - %g" | sed -e "s%^## %  - %g" | sed -e "s%^# %- %g"\
 	| sed -e "s%\(.*\)- \(.*\)%echo -n \"\1- [\2]\";echo \"(#\L\2)\" | tr ' ' '-' | tr -d '/' | tr -d '.' | tr -d ',' | sed -e 's/，//g'| tr -d ':' | tr -d '@'%g" | bash -v >>${README}.toc 2>/dev/null
 
 echo -e "\n<!-- toc end -->" >> ${README}.toc

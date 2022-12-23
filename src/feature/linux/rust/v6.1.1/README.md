75ddf81034c9c831df7878ab42dad5f1c055b66f
Falcon <falcon@tinylab.org>
2022-12-23 19:44:32 +0800

    feature: rust: add v6.1.1 support
    Signed-off-by: Falcon <falcon@tinylab.org>

---
+++ src/feature/linux/rust/v6.1.1/README.md
@@ -0,0 +1,4 @@
+
+Rust For Linux has been merged into v6.1+ Linux kernel.
+
+ref: <https://lore.kernel.org/rust-for-linux/>
---
+++ src/feature/linux/rust/v6.1.1/download.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+CURDIR=$(cd $(dirname $0)/ && pwd)
+
+# prepare necessary deps
+LABDIR=/labs/linux-lab
+
+which clang >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "LOG: Install missing llvm & clang"
+    $LABDIR/tools/deps/llvm.sh
+fi
+
+if [ ! -f "$HOME/.cargo/bin/rustc" -o ! -d "$HOME/.rustup/toolchains" ]; then
+    echo "LOG: Install missing rust environment"
+    $LABDIR/tools/deps/rust.sh
+    if [ $? -ne 0 ]; then
+        echo "ERR: Missing rust environment, please download it manually with"
+        echo
+        echo "    tools/deps/rust.sh"
+        echo
+        exit 1
+    fi
+fi
+
+# load rust env if required
+if [ -f "$HOME/.cargo/bin/rustc" -a -d "$HOME/.rustup/toolchains" ]; then
+    grep -i ".cargo" $HOME/.bashrc
+    if [ $? -ne 0 ]; then
+       # remove the old setting
+       sed -i -e "/.cargo\/env/d" $HOME/.bashrc
+       # add new with file checking
+       echo '[ -f "$HOME/.cargo/env" ] && source "$HOME/.cargo/env"' >> $HOME/.bashrc
+    fi
+fi
+
+pushd $CURDIR >/dev/null
+if [ ! -f feature.downloaded ]; then
+    touch feature.downloaded
+fi
+popd >/dev/null

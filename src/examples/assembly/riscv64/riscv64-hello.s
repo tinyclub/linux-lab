d50bdd99d3fca36d8f41dca8627b255ee1cbadca
Zhangjin Wu <falcon@tinylab.org>
2023-03-01 22:14:03 +0800

    examples: asm: use pc relative addressing
    PC relative addressing works in M mode, but this still not work if
    reboot syscall added.
    
    Signed-off-by: Zhangjin Wu <falcon@tinylab.org>

--- src/examples/assembly/riscv64/riscv64-hello.s
+++ src/examples/assembly/riscv64/riscv64-hello.s
@@ -3,25 +3,24 @@
 #      __NR_write, __NR_exit defined include/uapi/asm-generic/unistd.h
 #
 
-    .section .rodata
-msg:
-    .string "Hello, RISC-V 64!\n"
-    len = . - msg                # to use 'li', this .rodata section must be put before the .text section in this file, gcc 9.3
-                                 # otherwise, we should use 'lw' with "len: .word . - msg"
-
     .text
     .globl _start
 _start:
                                  # write(1, msg, len)
     li a0, 1                     # write to stdout
-    lui a1,       %hi(msg)       # load msg(hi)
-    addi a1, a1,  %lo(msg)       # load msg(lo)
-    li a2, len                   # length of the msg
+    la a1, msg                   # load msg
+    lw a2, len                   # length of the msg
 
     li a7, 64                    # __NR_write
     ecall
 
-                                 # exit(0)
     li a0, 0                     # return 0
     li a7, 93                    # __NR_exit
     ecall
+
+    .section .rodata
+msg:
+    .string "Hello, RISC-V 64!\n"
+
+len:
+    .word . - msg

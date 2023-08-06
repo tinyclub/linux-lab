db3376e1e93661ff0ac89bdd2e8e4195fd59e01d
Zhangjin Wu <falcon@tinylab.org>
2023-08-07 01:57:03 +0800

    nolibc: add crosstool download script
    Signed-off-by: Zhangjin Wu <falcon@tinylab.org>

---
+++ tools/nolibc/crosstool.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# crosstool.sh -- download crosstool from https://mirrors.edge.kernel.org/pub/tools/crosstool/
+#
+# Copyright (C) 2023 Zhangjin Wu <falcon@tinylab.org>
+#
+# mirror sites:
+#
+# 1. https://cdn.kernel.org/pub/tools/crosstool/
+# 2. https://mirrors.ustc.edu.cn/kernel.org/tools/crosstool/
+#
+
+VERSION=13.2.0
+MIRROR=https://mirrors.ustc.edu.cn/kernel.org/tools/crosstool/
+HOST=`uname -m`
+TOOLCHAINS="aarch64-linux arm-linux-gnueabi loongarch64-linux mips64-linux powerpc64-linux riscv64-linux s390-linux x86_64-linux"
+
+for toolchain in ${TOOLCHAINS[@]}; do
+  toolchain_url=$MIRROR/files/bin/${HOST}/${VERSION}/${HOST}-gcc-${VERSION}-nolibc-${toolchain}.tar.xz
+
+  echo -e "Downloading ${toolchain}-gcc from ${toolchain_url}\n"
+  wget -c ${toolchain_url}
+  if [ $? -eq 0 ]; then
+    mkdir -p toolchains
+    echo -e "Decompressing ${toolchain}-gcc to $PWD/toolchains\n"
+
+    tar xf x86_64-gcc-${VERSION}-nolibc-${toolchain}.tar.xz -C toolchains
+    [ $? -eq 0 ] && TOOLCHAIN_PATH=$PWD/toolchains/gcc-${VERSION}-nolibc/${toolchain}/bin/:$TOOLCHAIN_PATH
+  fi
+done
+
+echo "Toolchains for ${TOOLCHAINS} are stored in $PWD/toolchains."
+echo -e "Run and save the following line to your shell configuration file:\n"
+echo export PATH=$TOOLCHAIN_PATH\$PATH

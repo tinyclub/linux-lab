afb3f35fb1260d206f547cb6fd7b2ea32c65601f
Zhangjin Wu <falcon@tinylab.org>
2023-08-07 12:58:36 +0800

    nolibc: crosstool.sh: allow setting from env variables
    Signed-off-by: Zhangjin Wu <falcon@tinylab.org>

--- tools/nolibc/crosstool.sh
+++ tools/nolibc/crosstool.sh
@@ -10,22 +10,27 @@
 # 2. https://mirrors.ustc.edu.cn/kernel.org/tools/crosstool/
 #
 
-VERSION=13.2.0
-MIRROR=https://mirrors.ustc.edu.cn/kernel.org/tools/crosstool/
-HOST=`uname -m`
-TOOLCHAINS="aarch64-linux arm-linux-gnueabi loongarch64-linux mips64-linux powerpc64-linux riscv64-linux s390-linux x86_64-linux"
+[ -z "$VERSION" ] && VERSION=13.2.0
+[ -z "$MIRROR" ] && MIRROR=https://mirrors.ustc.edu.cn/kernel.org/tools/crosstool/
+[ -z "$HOST" ] && HOST=`uname -m`
+[ -z "$TOOLCHAINS" ] && TOOLCHAINS="aarch64-linux arm-linux-gnueabi loongarch64-linux mips64-linux powerpc64-linux riscv64-linux s390-linux x86_64-linux"
+[ -z "$EXPORT" ] && EXPORT=0
 
 for toolchain in ${TOOLCHAINS[@]}; do
   toolchain_url=$MIRROR/files/bin/${HOST}/${VERSION}/${HOST}-gcc-${VERSION}-nolibc-${toolchain}.tar.xz
 
-  echo -e "Downloading ${toolchain}-gcc from ${toolchain_url}\n"
-  wget -c ${toolchain_url}
-  if [ $? -eq 0 ]; then
-    mkdir -p toolchains
-    echo -e "Decompressing ${toolchain}-gcc to $PWD/toolchains\n"
+  if [ "$EXPORT" == "1" ]; then
+    TOOLCHAIN_PATH=$PWD/toolchains/gcc-${VERSION}-nolibc/${toolchain}/bin/:$TOOLCHAIN_PATH
+  else
+    echo -e "Downloading ${toolchain}-gcc from ${toolchain_url}\n"
+    wget -c ${toolchain_url}
+    if [ $? -eq 0 ]; then
+      mkdir -p toolchains
+      echo -e "Decompressing ${toolchain}-gcc to $PWD/toolchains\n"
 
-    tar xf x86_64-gcc-${VERSION}-nolibc-${toolchain}.tar.xz -C toolchains
-    [ $? -eq 0 ] && TOOLCHAIN_PATH=$PWD/toolchains/gcc-${VERSION}-nolibc/${toolchain}/bin/:$TOOLCHAIN_PATH
+      tar xf x86_64-gcc-${VERSION}-nolibc-${toolchain}.tar.xz -C toolchains
+      [ $? -eq 0 ] && TOOLCHAIN_PATH=$PWD/toolchains/gcc-${VERSION}-nolibc/${toolchain}/bin/:$TOOLCHAIN_PATH
+    fi
   fi
 done
 
